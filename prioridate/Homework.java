b3e98755e483bd69c04923a1d29a2932f78c4e5e
Jordyn McComber <jordynmccomber@Jordyns-MacBook-Air.local>
2021-06-01 21:11:59 -0400

    created stubs for , , , , and

--- prioridate/Homework.java
+++ prioridate/Homework.java
@@ -1,5 +1,23 @@
 package prioridate;
 
-public class Homework {
-    
+public class Homework extends Assignment {
+    private int numQuestions;
+
+    public Homework(String title, String subject, String dueDate, double percentage, boolean isCompleted, int priority, int numQuestions) {
+        super(title, subject, dueDate, percentage, isCompleted, priority);
+        this.numQuestions = numQuestions;
+    }
+
+    public double getPercentage() {
+        return this.percentage;
+    }
+
+    public String getDueDate() {
+        return this.dueDate;
+    }
+
+    public int getPriority() {
+        int priority = 0;
+        return priority;
+    }
 }
--- prioridate/Reading.java
+++ prioridate/Reading.java
@@ -1,5 +1,25 @@
 package prioridate;
 
-public class Reading {
-    
+public class Reading extends Assignment {
+    private String[] chapters;
+    private int numPages;
+
+    public Reading(String title, String subject, String dueDate, double percentage, boolean isCompleted, int priority, String[] chapters, int numPages) {
+        super(title, subject, dueDate, percentage, isCompleted, priority);
+        this.chapters = chapters;
+        this.numPages = numPages;
+    }
+
+    public double getPercentage() {
+        return this.percentage;
+    }
+
+    public String getDueDate() {
+        return this.dueDate;
+    }
+
+    public int getPriority() {
+        int priority = 0;
+        return priority;
+    }
 }
