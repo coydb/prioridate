60cf7f974e6491bdf27a6dd498cc68f79ede8d6f
CynLeon <kythosl4@gmail.com>
2021-06-01 20:48:30 -0400

    setting up Student and Class classes

--- prioridate/Class.java
+++ prioridate/Class.java
@@ -1,5 +1,43 @@
 package prioridate;
 
-public class Class {
+import java.util.ArrayList;
+
+public class Class 
+{
+    protected ArrayList<Student> students;
+    protected ArrayList<Assignment> assignments;
+    protected Teacher teacher;
+    private String name;
+
+    public void viewStudents(Student students)
+    {
+
+    }
+
+    public void teacherInfo(Teacher teacher)
+    {
+
+    }
+
+    public void viewStudentsCompletion()
+    {
+
+    }
+
+    public void addStudent()
+    {
+
+    }
+
+    public void addModule()
+    {
+
+    }
+
+    public void addGroup()
+    {
+
+    }
     
+
 }
--- prioridate/Student.java
+++ prioridate/Student.java
@@ -1,5 +1,38 @@
 package prioridate;
 
-public class Student {
-    
+import java.util.ArrayList;
+
+public class Student 
+{
+    private String name;
+    private double gradePercent;
+    private double completionPercent;
+    protected ArrayList<Assignment> assignmentsCompleted;
+    protected ArrayList<Assignment> assignmentsDue;
+
+    private void viewAssignmentsCompleted(Assignment assignmentCompleted)
+    {
+
+    }
+
+    private void viewAssignmentsDue(Assignment assignmentsDue)
+    {
+
+    }
+
+    public double calculateCompletionPercent(Assignment assignmentsDue)
+    {
+
+    }
+
+    public double calculateGradePercent(Assignment assignmentsDue, Assignment assignmentsCompleted)
+    {
+
+    }
+
+    public void checkOffAssignment(Assignment assignmentsDue)
+    {
+        
+    }
+
 }
