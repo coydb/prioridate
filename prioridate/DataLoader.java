0515631a2c23e0cc905812cd88b15aad3b944d95
coydb <coy.dr.burke@gmail.com>
2021-06-02 14:59:17 -0400

    Added default behavior for DataLoader.java

--- prioridate/DataLoader.java
+++ prioridate/DataLoader.java
@@ -4,16 +4,20 @@ import java.util.ArrayList;
 
 public class DataLoader {
   public ArrayList<Student> getStudents() {
-    
+    // default behavior to allow compilation
+    return new ArrayList<Student>();
   }
   public ArrayList<Teacher> getTeachers() {
-
+    // default behavior to allow compilation
+    return new ArrayList<Teacher>();
   }
   public ArrayList<Course> getCourses() {
-
+    // default behavior to allow compilation
+    return new ArrayList<Course>();
   }
   public ArrayList<Assignment> getAssignments() {
-    
+    // default behavior to allow compilation
+    return new ArrayList<Assignment>();   
   }
     
 }
