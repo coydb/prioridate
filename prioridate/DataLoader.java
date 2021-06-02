5b55a258c19e73c99f095bd7356b439811eb3c97
coydb <coy.dr.burke@gmail.com>
2021-06-02 14:57:16 -0400

    Added default behavior for Lists

--- prioridate/DataLoader.java
+++ prioridate/DataLoader.java
@@ -1,4 +1,19 @@
 package prioridate;
+
+import java.util.ArrayList;
+
 public class DataLoader {
+  public ArrayList<Student> getStudents() {
+    
+  }
+  public ArrayList<Teacher> getTeachers() {
+
+  }
+  public ArrayList<Course> getCourses() {
+
+  }
+  public ArrayList<Assignment> getAssignments() {
+    
+  }
     
 }
