758329e4a76ded16b8560054475cb3f68b258233
coydb <coy.dr.burke@gmail.com>
2021-06-02 14:08:44 -0400

    Changed Class.java to Course.java

---
+++ prioridate/Course.java
@@ -0,0 +1,43 @@
+package prioridate;
+
+import java.util.ArrayList;
+
+public class Course 
+{
+    protected ArrayList<Student> students;
+    protected ArrayList<Assignment> assignments;
+    protected Teacher teacher;
+    private String name;
+
+    public void viewStudents(Student students)
+    {
+
+    }
+
+    public void teacherInfo(Teacher teacher)
+    {
+
+    }
+
+    public void viewStudentsCompletion()
+    {
+
+    }
+
+    public void addStudent()
+    {
+
+    }
+
+    public void addModule()
+    {
+
+    }
+
+    public void addGroup()
+    {
+
+    }
+    
+
+}
