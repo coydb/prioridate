80261eded0a680c8a58cf480bf6d6faa6e441b8d
CynLeon <kythosl4@gmail.com>
2021-06-09 18:59:31 -0400

    javadoc for Teacher Student and Course

--- prioridate/Course.java
+++ prioridate/Course.java
@@ -2,59 +2,57 @@ package prioridate;
 
 import java.util.ArrayList;
 
+/**
+ * Class that creates template for Course classes.  
+ */
 public class Course 
 {
     private int courseId;
     private String className;
     private ArrayList<Assignment> assignments;
 
+    /**
+     * @param courseId ID of course
+     * @param className Name of course
+     * @param assignments assignments in course
+     */
     public Course(int courseId, String className, ArrayList<Assignment> assignments)
     {
         this.courseId = courseId;
         this.className = className;
         this.assignments = new ArrayList<Assignment>(assignments);
     }
-
-    public void viewStudents()
-    {
-        
-    }
-
-    public void teacherInfo(Teacher teacher)
-    {
-
-    }
-
-    public void viewStudentsCompletion()
-    {
-
-    }
-
-    public void addStudent()
-    {
-
-    }
-
-    //public Student getStudent(String name)
-    //{
-
-    //}
     
+    /**
+     * Gets ID of course
+     * @return returns course ID
+     */
     public int getCourseId()
     {
         return this.courseId;
     }
 
+    /**
+     * Gets name of class
+     * @return returns class's name
+     */
     public String getClassName()
     {
         return this.className;
     }
 
+    /**
+     * Gets list of assignments
+     * @return returns array of assignments for course. 
+     */
     public ArrayList<Assignment> getAssignments()
     {
         return this.assignments;
     }
 
+    /**
+     * Loop which returns information about the Course. 
+     */
     public String toString() 
     {
         String outString = "\nCourse Id: " + courseId;
@@ -66,7 +64,4 @@ public class Course
         }
         return outString;
     }
-
-
-    
 }
